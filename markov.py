4d99d10c0f4fb8ad950c5f9af4cced14a100bd34
Guilherme Caulada (Sighmir) <guistoppa1995@gmail.com>
2018-11-20 12:23:42 -0600

    Improves message generation from POS tags

--- markov.py
+++ markov.py
@@ -1,17 +1,19 @@
 from markovify import Text as T
+from difflib import SequenceMatcher
 import nltk
 import re
 
 class Text(T):
-  def make_short_sentence_with(self, message, max_chars, min_chars=0, strict=False, **kwargs):
-    tries = kwargs.get('tries', 10)
+  def make_short_sentence_with(self, message, max_chars, min_chars=0, **kwargs):
+    tries = kwargs.get('tries', 50)
+    similarity = kwargs.get('similarity', 0.5)
     tokens = nltk.word_tokenize(message)
-    for w, t in nltk.pos_tag(tokens):
-      if (t[0] == 'N' or t[0] == 'R' or t[0] == 'V'):
-        for _ in range(tries):
-          sentence = self.make_sentence_with_start(w,strict,**kwargs)
-          if sentence and len(sentence) <= max_chars and len(sentence) >= min_chars:
-            return sentence
+    keywords = [w for w, t in nltk.pos_tag(tokens) if (t[0] == 'N' or t[0] == 'R' or t[0] == 'V')]
+    for _ in range(tries):
+      sentence = self.make_short_sentence(max_chars, min_chars,**kwargs)
+      used_kw = [w for w in sentence.split() if w in keywords]
+      if len(used_kw)/len(keywords) > similarity:
+        return sentence
 
 class NewlineText(Text):
   def sentence_split(self, text):
